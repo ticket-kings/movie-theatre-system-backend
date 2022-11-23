fc03fff3ba928618d890728f932fa690d88bdac5
Trevor Le <trevor.le1@ucalgary.ca>
2022-11-23 13:45:26 -0700

    Created Spring Boot project

---
+++ src/main/java/org/ticketkings/movietheatresystem/MovieTheatreSystemApplication.java
@@ -0,0 +1,13 @@
+package org.ticketkings.movietheatresystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTheatreSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTheatreSystemApplication.class, args);
+	}
+
+}
