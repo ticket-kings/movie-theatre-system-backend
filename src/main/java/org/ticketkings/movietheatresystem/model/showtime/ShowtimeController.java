44b61fff90775ad252a4e16633e4ff942c6451cd
Trevor Le <trevor.le1@ucalgary.ca>
2022-11-25 07:36:58 -0700

    Created Showtime endpoint and exported postman collection

--- src/main/java/org/ticketkings/movietheatresystem/model/showtime/ShowtimeController.java
+++ src/main/java/org/ticketkings/movietheatresystem/model/showtime/ShowtimeController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1/showtime")
 public class ShowtimeController {
 
     private final ShowtimeService showtimeService;
