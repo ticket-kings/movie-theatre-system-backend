366e5a8bd6ba3b91486a4cba59203d7811216977
Trevor Le <trevor.le1@ucalgary.ca>
2022-11-26 17:23:28 -0700

    Implemented getMovie by id

--- src/main/java/org/ticketkings/movietheatresystem/model/movie/MovieController.java
+++ src/main/java/org/ticketkings/movietheatresystem/model/movie/MovieController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class MovieController {
     @GetMapping
     public List<Movie> getMovies() {
         return movieService.getMovies();
+    }
+
+    @GetMapping("{movieId}")
+    public Movie getMovie(@PathVariable Integer movieId) {
+        return movieService.getMovie(movieId);
     }
 }
--- src/main/java/org/ticketkings/movietheatresystem/model/movie/MovieService.java
+++ src/main/java/org/ticketkings/movietheatresystem/model/movie/MovieService.java
@@ -1,8 +1,11 @@
 package org.ticketkings.movietheatresystem.model.movie;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class MovieService {
@@ -17,4 +20,13 @@ public class MovieService {
         return movieRepository.findAll();
     }
 
+    public Movie getMovie(Integer movieId) {
+        Optional<Movie> optional = movieRepository.findById(movieId);
+
+        if (optional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Movie with ID: " + movieId + " does not exist");
+        }
+
+        return optional.get();
+    }
 }
