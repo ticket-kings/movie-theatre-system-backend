c481e4b8e95028d565f6fe6ea953b0cc774bf3e9
Trevor Le <trevor.le1@ucalgary.ca>
2022-11-30 12:14:06 -0700

    Implemented annual payments

--- src/main/java/org/ticketkings/movietheatresystem/model/payment/annual/AnnualPaymentService.java
+++ src/main/java/org/ticketkings/movietheatresystem/model/payment/annual/AnnualPaymentService.java
@@ -2,7 +2,12 @@ package org.ticketkings.movietheatresystem.model.payment.annual;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class AnnualPaymentService {
@@ -15,6 +20,31 @@ public class AnnualPaymentService {
 
     public List<AnnualPayment> getAnnualPayments() {
         return annualPaymentRepository.findAll();
+    }
+
+    public AnnualPayment createAnnualPayment(AnnualPayment annualPayment) {
+        annualPayment.setPaymentDate(new Date());
+        scheduleNextPayment(annualPayment);
+        return annualPaymentRepository.save(annualPayment);
+    }
+
+    private void scheduleNextPayment(AnnualPayment currentPayment) {
+        currentPayment.setNextPaymentDate(oneYearFromNow());
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+        scheduler.scheduleAtFixedRate(() -> {
+            AnnualPayment nextPayment = new AnnualPayment();
+            nextPayment.setAmount(currentPayment.getAmount());
+            nextPayment.setCardId(currentPayment.getCardId());
+            nextPayment.setNextPaymentDate(oneYearFromNow());
+            createAnnualPayment(nextPayment);
+        }, 365, 365, TimeUnit.DAYS);
+    }
+
+    private Date oneYearFromNow() {
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.YEAR, 1);
+        return cal.getTime();
     }
 
 }
--- src/main/java/org/ticketkings/movietheatresystem/model/user/registered/RegisteredUserController.java
+++ src/main/java/org/ticketkings/movietheatresystem/model/user/registered/RegisteredUserController.java
@@ -1,11 +1,15 @@
 package org.ticketkings.movietheatresystem.model.user.registered;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.ticketkings.movietheatresystem.model.card.Card;
 import org.ticketkings.movietheatresystem.model.card.CardService;
+import org.ticketkings.movietheatresystem.model.payment.annual.AnnualPayment;
+import org.ticketkings.movietheatresystem.model.payment.annual.AnnualPaymentController;
+import org.ticketkings.movietheatresystem.model.payment.annual.AnnualPaymentService;
 
 @RestController
 @RequestMapping(path = "api/v1/user/registered")
@@ -13,14 +17,17 @@ public class RegisteredUserController {
 
 	private final RegisteredUserService registeredUserService;
 	private final CardService cardService;
+	private final AnnualPaymentService annualPaymentService;
 
 	@Autowired
 	public RegisteredUserController(
 			RegisteredUserService registeredUserService,
-			CardService cardService
+			CardService cardService,
+			AnnualPaymentService annualPaymentService
 	) {
 		this.registeredUserService = registeredUserService;
 		this.cardService = cardService;
+		this.annualPaymentService = annualPaymentService;
 	}
 
 	@GetMapping("/login")
@@ -39,6 +46,11 @@ public class RegisteredUserController {
 
 		Card card = registeredUser.getCard();
 		cardService.createCard(card);
+
+		AnnualPayment annualPayment = new AnnualPayment();
+		annualPayment.setCardId(card.getId());
+		annualPayment.setAmount(20F);
+		card.setPayments(List.of(annualPaymentService.createAnnualPayment(annualPayment)));
 
 		registeredUser.setCardId(card.getId());
 		return registeredUserService.createRegisteredUser(registeredUser);
