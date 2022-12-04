1eba66df487bd7da819244f6a7491d18a55bce16
Trevor Le <trevor.le1@ucalgary.ca>
2022-12-03 21:27:17 -0700

    Completed documentation for cors and main class

--- src/main/java/org/ticketkings/movietheatresystem/CorsConfiguration.java
+++ src/main/java/org/ticketkings/movietheatresystem/CorsConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Cross-origin configuration class that allows API endpoints to be accessed by web browser
+ */
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
--- src/main/java/org/ticketkings/movietheatresystem/MovieTheatreSystemApplication.java
+++ src/main/java/org/ticketkings/movietheatresystem/MovieTheatreSystemApplication.java
@@ -3,6 +3,9 @@ package org.ticketkings.movietheatresystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class that starts and runs the Spring Boot application
+ */
 @SpringBootApplication
 public class MovieTheatreSystemApplication {
 
